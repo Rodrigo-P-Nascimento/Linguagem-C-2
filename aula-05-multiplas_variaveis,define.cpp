e62f8e3c862d83470e6b3913d24fff5612b9b952
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-01-21 09:48:09 -0300

    aula-5

---
+++ aula-05-multiplas_variaveis,define.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define PI 3.14
+    #define OLA cout << "OLa, pessoal!\n";
+
+int main(void){
+
+    int vidas,tiros,saude = 0;
+
+    cout << vidas <<"\n"<< tiros <<"\n"<< saude <<"\n"<< PI<<"\n";
+    OLA;
+
+    return 0;
+}
