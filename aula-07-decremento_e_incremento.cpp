956c78e1772fd5ca9d07a8ea5f2efc58637c1e23
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-01-21 11:16:27 -0300

    aula 7

---
+++ aula-07-decremento_e_incremento.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    int n1=0;
+
+    /*
+    Sao as formas de encremento e decremento do C
+
+    n1 = n1 + 1;
+    n1+=1
+    n1++ ou ++n1
+
+    n1++(acontece o acrescimo na linha seguinte)
+    ++n1(acontece o acrescimo na propria linha)
+    */
+
+    cout << n1++ << "\n";
+    cout << ++n1 << "\n";
+    cout << n1;
+
+    return 0;
+}
