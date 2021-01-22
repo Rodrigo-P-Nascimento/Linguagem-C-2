57007c6e2de815646302235449459edec2f77eaf
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-01-22 09:04:45 -0300

    aula 8

---
+++ aula-08-invertendo_valores_de_variaveis.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    int num = 10;
+
+    cout << num <<"\n";
+
+    //num *= -1;
+
+    num = -num;
+
+    //cout << -num <<"\n";  inversao momentanea
+    cout << num <<"\n";
+
+    return 0;
+}
