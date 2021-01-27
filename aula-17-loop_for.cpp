f2b76fa804af9363f671ea54374f774a08d78542
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-01-27 09:20:29 -0300

    aula 17

---
+++ aula-17-loop_for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    int cont;
+
+    for(cont = 0; cont <= 10; cont++){
+        cout << "Numero: "<< cont << "\n";
+    }
+
+
+    return 0;
+}
