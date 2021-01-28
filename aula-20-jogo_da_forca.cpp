91abdf085a635e24fcb04ba789f7340f9b010759
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-01-28 08:46:35 -0300

    Update aula-20-jogo_da_forca.cpp

--- aula-20-jogo_da_forca.cpp
+++ aula-20-jogo_da_forca.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main(){
 
-    char palavra[] = "bolo";
-    int tamArray = sizeof(palavra);
+    char palavra[] = "bola";
+    int tamArray = sizeof(palavra)-1;
     char auxilioPalavra[tamArray];
     int i, tentativas = 5;
     int acertos=0, palavraCompleta=0;
     char letra;
 
-    for(i=0;i < tam; i++){
+    for(i=0;i < tamArray; i++){
+            auxilioPalavra[i] = '-';
+        }
+
+    while(1){
+        printf("Bem-vindo ao nosso pequeno jogo da forca.\n");
+        printf("A palavra ja foi decidida, ela contem %d letras.\n", int(sizeof(palavra)-1));
+
+        for(i=0;i < sizeof(auxilioPalavra); i++){
             cout << auxilioPalavra[i];
         }
 
-    printf("Bem-vindo ao nosso pequeno jogo da forca.\n");
-    printf("A palavra ja foi decidida, ela contem %d letras.\n", int(sizeof(palavra)-1));
-
-    while(1){
         printf("\nNumero de tentativas: %d\n", tentativas);
         printf("\nQual o seu palpite: ");
         cin >> letra;
 
-        for(i=0; i < sizeof(palavra); i++){
+        for(i=0; i < tamArray; i++){
             if(letra == palavra[i] && letra != auxilioPalavra[i]){
                 acertos++;
                 palavraCompleta++;
@@ -38,18 +43,18 @@ int main(){
             printf("Palpite ERRADO!\n");
             tentativas--;
         }
-        for(i=0;i < sizeof(auxilioPalavra); i++){
-            cout << auxilioPalavra[i];
-        }
+        sleep(2);
+        system("clear");
         acertos = 0;
         if(tentativas == 0){
-            printf("\nGAME OVER\n");
+            printf("GAME OVER\n");
             break;
         }
         if(palavraCompleta == (sizeof(palavra)-1)){
-            printf("\nMuito bem vc achou a palavra!\n");
+            printf("Muito bem vc achou a palavra!\n");
             break;
         }
+
     }
     printf("A palavra era: ");
     for(i=0;i < sizeof(palavra); i++){
