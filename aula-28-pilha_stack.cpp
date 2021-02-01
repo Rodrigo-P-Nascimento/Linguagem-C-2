dd9d79718b8c5cf7a3e9a4b1c0c95ba4234ffbca
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-01 10:04:37 -0300

    Update aula-28-pilha_stack.cpp

--- aula-28-pilha_stack.cpp
+++ aula-28-pilha_stack.cpp
@@ -2,26 +2,35 @@
 #include <stack>
 
 using namespace std;
-//o ultimo elemento colocado na pilha eh o ultimo a ser removido da pilha
+/*
+
+O ultimo elemento colocado na pilha eh o ultimo a ser removido da pilha
+
+.push - adiciona elementos na pilha
+.pop  - apaga o ultimo elemento da pilha
+.size - mostra a quantidade de elementos da pilha
+.empty- retorna TRUE ou FALSE, ou seja, se tiver algo na pilha retorna TRUE se nao FALSE
+.top  - retorna o ultimo elemento adicionado na pilha
+*/
 int main(){
 
     stack <string> cartas;
 
-    //o metodo push adiciona elementos na pilha
     cartas.push("Rei de Copas");
     cartas.push("Rei de Espadas");
     cartas.push("Rei de Ouros");
     cartas.push("Rei de Paus");
 
     cout << "Tamanho da pilha: "<< cartas.size() << "\n";
+    cout << "Carta do topo: " <<cartas.top() << "\n";
 
-    cout << "Carta do topo: " <<cartas.top() << "\n"; //top mostra o ultimo elemento adicionado na pilha
+    cartas.pop();
 
-    cartas.pop();//metodo pop faz a retirada do ultimo elemento adicionado
-
-    //cout << "Carta do topo: " <<cartas.top() << "\n";
-
-
-
+    if(cartas.empty()){
+        cout << "A pilha esta vazia!\n";
+    }else{
+        cout << "A pilha nao esta vazia!\n";
+        cout << "Tamanho da pilha: "<< cartas.size() << "\n";
+    }
 	return 0;
 }
