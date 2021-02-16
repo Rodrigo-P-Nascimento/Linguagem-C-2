2b27636019e0c90b58771fe19618bae25159b986
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-16 09:00:38 -0300

    aula 50

--- aula-50-operando_arquivos-ofstream.cpp
+++ aula-50-operando_arquivos-ofstream.cpp
@@ -12,8 +12,24 @@ int main(){
     ofstream arquivo;
 
     arquivo.open("aula-50-texto.txt");
+    arquivo << "Teste numero 1\n";
+    arquivo.close();
 
-    arquivo << "Teste numero 1";
+    /*
+    Oh se liga, toda vez que vc abrir o arquivo txt e mandar algo escrever nele,
+    o programa vai sobrescrever o que ja tem no arquivo.
+
+    Pra deixar oq ja foi escrito nele basta colocar um parametro no metodo open()
+    open(nome_do_arquivo, ios::app);
+
+    No caso ele muda o cursor para o final do arquivo blz
+    */
+
+    arquivo.open("aula-50-texto.txt", ios::app);
+    //arquivo << "Teste numero 3\n";
+    //arquivo << "Teste numero 4\n";
+    //arquivo << "Teste numero 5\n";
+    arquivo.close();
 
 	return 0;
 }
