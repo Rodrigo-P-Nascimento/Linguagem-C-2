5f3a6df01d908fda7bf28b6b2401cd77fcf6c035
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-17 09:31:52 -0300

    aula 53

--- aula-53-iterator.cpp
+++ aula-53-iterator.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
 
+    vector <string>produtos = {"mouse", "teclado", "monitor","gabinete", "cx.som"};
+    vector <string>::iterator it;
 
+    //it=produtos.begin();
+    //it=produtos.end()-1;
+
+    //advance();-ela avanca uma quantida X de casas de uma coleção
+    //next();-vc usa direto no cout
+    //prev();
+
+    //advance(it, 1);
+    //cout << *it <<endl;
+
+    //cout << *next(it, 3) <<endl;
+
+    //cout << *prev(it, 0) <<endl;
+
+    for(it=produtos.begin();it!=produtos.end();it++){
+        cout << *it <<endl;
+    }
 
 	return 0;
 }
