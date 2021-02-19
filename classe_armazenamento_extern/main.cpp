991f7f9ab5da6ea645a208e0d7a7de9c5174552d
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-19 08:43:58 -0300

    aula 57

---
+++ classe_armazenamento_extern/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "variaveis.h"
+
+using namespace std;
+
+int num{190};
+extern int valor;
+
+void impNum();
+
+int main(){
+    cout << valor << endl;
+    cout << tempo << endl;
+    impNum();
+    return 0;
+}
