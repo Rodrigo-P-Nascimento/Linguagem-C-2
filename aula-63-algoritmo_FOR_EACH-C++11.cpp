aada3ab8ebe6606bed5cda7a55d6fc4d0a2a34b2
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-23 08:26:46 -0300

    aual 63

---
+++ aula-63-algoritmo_FOR_EACH-C++11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    vector <int>n{0,1,2,3,4,5,6,7,8,9};
+
+    for_each(n.begin(), n.end(),[](int num){cout << num << endl;});
+
+    return 0;
+}
