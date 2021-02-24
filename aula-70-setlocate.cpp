6c38d6ca54dc4ddb8e2eda70fd7c82f235d1d08e
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-24 09:52:05 -0300

    aula 70

---
+++ aula-70-setlocate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "portuguese");
+    cout << "Meu coração por ti bate mamãe!\n";
+
+
+
+    return 0;
+}
