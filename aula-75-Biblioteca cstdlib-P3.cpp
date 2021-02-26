755b37c3c3d7a6726cc485a260dcc0c8b55c88d6
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-26 10:42:47 -0300

    aula 75

---
+++ aula-75-Biblioteca cstdlib-P3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    /*==============================> ARITMÉTICA DE INTEIROS <====================
+
+    int n=-10;
+    int numerador, denominador;
+    div_t res;
+
+    numerador=10;
+    denominador=3;
+    res=div(numerador, denominador);
+
+    cout << abs(n) << endl;
+    cout << res.quot << " " <<res.rem << endl;
+    --------------------------------------------------------------------------------
+    abs > Valor absoluto (função)
+    div > Divisão integral (função)
+    labs > Valor absoluto (função)
+    ldiv > Divisão integral (função)
+    llabs > Valor absoluto (função)
+    lldiv > Divisão integral (função)
+    ===============================================================================*/
+
+
+    return 0;
+}
