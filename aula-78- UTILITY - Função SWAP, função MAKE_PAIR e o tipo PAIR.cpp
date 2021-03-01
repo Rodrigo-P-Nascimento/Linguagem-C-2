3a9a20b472322a2358e4ff75619a7ad03c7a6188
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-03-01 09:55:59 -0300

    aula 78

---
+++ aula-78- UTILITY - Função SWAP, função MAKE_PAIR e o tipo PAIR.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+int main(){
+    /*======================> SWAP <=================================
+    int a=10, b=15;
+    cout << "Valor de 'a': " << a << "|Valor de 'b': " << b << endl;
+    swap(a, b);//ela troca os valores das variavies
+    cout << "Valor de 'a': " << a << "|Valor de 'b': " << b << endl;
+    ===============================================================*/
+    /*======================> PAIR <=================================
+    pair<int,char>par1;
+    pair<int, string>par2;
+
+    par1=make_pair(10, 'a');
+    par2=make_pair(1, "Mauro de Souza");
+
+    cout << "Par 1: "<<par1.first << " - " << par1.second << endl;
+    cout << "Par 2: "<<par2.first << " - " << par2.second << endl;
+
+    -----------------------------------------------------------------
+    swap > Valores de troca de dois objetos (modelo de função)
+    make_pair > Construir objeto de par (modelo de função)
+    forward > Argumento de encaminhamento (modelo de função)
+    move > Mover como rvalue (modelo de função)
+    move_if_noexcept > Mova se noexcept (modelo de função)
+    declval > Valor de declaração (modelo de função)
+    ===============================================================*/
+    return 0;
+}
