af78de03e17a24784817f783c031f7ff44e8aae8
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-03-02 10:18:22 -0300

    aula 81

---
+++ aula-81-Biblioteca cctype.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main(){
+    /*=============================> FUNCOES <======================
+
+    isalnum > Verifique se o caractere é alfanumérico (função)
+    isalpha > Verifique se o caractere é alfabético (função)
+    isblank > Verifique se o caractere está em branco (função)
+    iscntrl > Verifique se o caractere é um caractere de controle (função)
+    isdigit > Verifique se o caractere é um dígito decimal (função)
+    isgraph > Verifique se o personagem tem representação gráfica (função)
+    islower > Verifique se o caractere é uma letra minúscula (função)
+    isprint > Verifique se o caractere pode ser impresso (função)
+    ispunct > Verifique se o caractere é um caractere de pontuação (função)
+    isspace > Verifique se o caractere é um espaço em branco (função)
+    isupper > Verifique se o caractere é uma letra maiúscula (função)
+    isxdigit > Verifique se o caractere é um dígito hexadecimal (função)
+
+    tolower > Converter letras maiúsculas em minúsculas (função)
+    toupper > Converter letras minúsculas em maiúsculas (função)
+    ===============================================================*/
+
+    return 0;
+}
+
