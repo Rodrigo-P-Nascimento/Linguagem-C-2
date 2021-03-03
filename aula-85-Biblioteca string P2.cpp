0d17131bbcd63089fc2a305079c87f09a4412801
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-03-03 10:39:51 -0300

    Update aula-85-Biblioteca string P2.cpp

--- aula-85-Biblioteca string P2.cpp
+++ aula-85-Biblioteca string P2.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main(){
 
+    string txt("Teste de string");
+    int tam=txt.size();
+
+    /*
+    for(int i=0;i<tam;i++){
+        cout << txt.at(i);
+    }
+
+    cout << endl;
+    cout <<"Ultima letra: " << txt.back() << "\nPrimeiro elemento: "<<txt.front() << endl;
+
+    txt.front() = 'F';
+    txt.back() = 'e';
+    cout <<"Ultima letra: " << txt.back() << "\nPrimeiro elemento: "<<txt.front() << endl;
+    */
+
+    txt+=" - Teste da string 2";
+    txt.assign("Substituto");
 
 
 
+    cout << txt;
 
     return 0;
 }
