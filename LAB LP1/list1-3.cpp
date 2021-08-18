c901dcd4a854f16e57aba294bab7db71529e6a59
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-08-18 14:11:03 -0300

    Q 3 FEITA

--- LAB LP1/list1-3.cpp
+++ LAB LP1/list1-3.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <iomanip>
 
-
+using namespace std;
+/*
+(Médio) Sabe-se que o quilowatt de energia custa um quinto do salário mínimo. Faça um programa que receba o valor do salário mínimo e a quantidade de quilowatts gasta por uma residência. Calcule e imprima:
+a) O valor, em reais, de cada quilowatt
+b) O valor, em reais, a ser pago por essa residência
+c) O valor a ser pago por essa residência com um desconto de 15%
+*/
 int main(){
 
     //kw = 1/5 salario minimo
 
-    float salario;
+    float salario, kiloSalario, valorPago;
     int quilowatts;
 
-    
+    cin >> salario >> quilowatts;
+    kiloSalario = salario * 0.2;
+    valorPago = kiloSalario*quilowatts;
+
+    cout << fixed << setprecision(2);
+    cout << "Cada quilowatt vale R$: " << kiloSalario << endl;
+    cout << "Valor a ser pago pela residencia R$: "<< valorPago << endl;
+    cout << "Valor a ser pago com desconto de 15% R$: " << valorPago - ((valorPago*15) / 100) << endl;
 
     return 0;
 }
