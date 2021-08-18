b8c7379fe3f2c2547c29cadbe257a78627f38944
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-08-18 14:40:51 -0300

    Q 7 FEITA

---
+++ LAB LP1/list1-7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+/*
+Efetuar a leitura de um valor numérico inteiro e apresentar o resultado do valor lido elevado ao quadrado.
+*/
+
+int main(){
+
+    int num;
+
+    cout << "Digite um valor inteiro: ";
+    cin >> num;
+
+    cout << "Resultado de " << num << " ao qaudrado: " << num* num << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
