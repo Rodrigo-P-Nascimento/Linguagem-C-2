a17fe09a0ba32ee3831485336591cd504712be9f
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-08-19 10:13:07 -0300

    Update list1-11.cpp

--- LAB LP1/list1-11.cpp
+++ LAB LP1/list1-11.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 /*
+(Difícil) Em uma eleição sindical concorreram aos cargos de presidente três candidatos (representados pelas variáveis A, B e C). Durante a apuração dos votos foram computados votos nulos e em branco, além dos votos válidos para cada candidato. Deve ser criado um programa para fazer a leitura da quantidade de votos válidos para cada candidato, além de ler também a quantidade de votos nulos e em branco. Ao final, o programa deve apresentar o número total de eleitores, considerando votos válidos, nulos e em branco; o percentual correspondente aos votos válidos em relação à quantidade de eleitores; o percentual correspondente de votos válidos do candidato A em relação à quantidade de eleitores; o percentual correspondente de votos válidos do candidato B em relação à quantidade de eleitores; o percentual correspondente de votos válidos do candidato C em relação à quantidade de eleitores; o percentual correspondente de votos nulos em relação à quantidade de eleitores; e por último o percentual correspondente de votos em branco em relação à quantidade de eleitores.
 */
 
 int main(){
