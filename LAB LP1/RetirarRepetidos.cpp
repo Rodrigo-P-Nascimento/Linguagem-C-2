e3424355e4e32d1d2d90f0dbf76ac85748b8fc33
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-08-25 15:12:00 -0300

    up

--- LAB LP1/RetirarRepetidos.cpp
+++ LAB LP1/RetirarRepetidos.cpp
@@ -6,31 +6,29 @@ int main(){
 
     int tamVetor, i, j, num;
 
-    cout << "Digita um valor ae: ";
     cin >> tamVetor;
 
-    int vetorA[tamVetor];
-    int vetorB[tamVetor];
+    int vetorA[tamVetor]; //Crianção do VetorA
 
     for(i=0; i < tamVetor; i++){
         cin >> num;
         vetorA[i] = num;
-        vetorB[i] = num;
     }
 
-    for(i=0;i < tamVetor; i++){
-        for(j=0; j < tamVetor; j++){
-            if(vetorA[i] == vetorB[j]){
-                vetorA[i] = 0;
+    for(i=0;i < tamVetor; i++){ //Se um valor na posição i tiver um clone na posição j, ele muda o valor da posução j para 0
+        for(j=i+1; j < tamVetor; j++){
+            if(vetorA[i] == vetorA[j]){
+                vetorA[j] = 0;
             }
         }
     }
 
-    for(i=0; i < tamVetor; i++){
-        cout << vetorA[i] << " ";
-        if(i == (tamVetor-1)) cout << endl;
+    for(i=0; i < tamVetor; i++){ //Valores com 0 não são mostrados 
+        if(vetorA[i] != 0){
+            cout << vetorA[i] << " ";
+            if(i == (tamVetor-1)) cout << endl;
+        }
     }
     
-  
     return 0;
 }
