32fe131171ab1f6142d5be66fb74101838791cc2
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-08-26 14:34:22 -0300

    Create list2-9.cpp

---
+++ LAB LP1/list2-9.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+using namespace std;
+
+/*
+(Difícil) Elabore uma função que calcule o valor aproximado de π através da série:
+
+O número de termos da série deverá ser fornecido pelo usuário. Escrever um programa que receba o número
+de termos da série necessário para calcular o valor aproximado de π. O cálculo da potenciação deverá ser
+introduzido através da função potencia(base, expoente), a ser criada pelo programador.
+
+*/
+
+
+int main(){
+
+    int num, i;
+    float pi=0;
+
+    cout << "Digite o valor de N para o tamanho da serie: ";
+    cin >> num;
+
+    for(i=0; i < num; i++){
+        pi += 
+    }
+
+    return 0;
+}
