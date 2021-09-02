d0f064a8ae610aee3813b602184b4a433fef9c4b
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-09-02 10:56:37 -0300

    up

---
+++ LAB LP1/OOSistemGasto.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Despensa{
+private:
+    string nome;
+    float valor;
+    string tipoDeGasto;
+public:
+    Despensa();
+
+    //Conjuntos de seters
+    void setNome(string nm){
+        nome = nm;
+    }
+    void setTipoDeGasto(string tc){
+        nome = tc;
+    }
+    void setValor(float vl){
+        valor = vl;
+    }
+
+    //Conjunto de geters
+    string getNome(){
+        return nome;
+    }
+
+    string getTipo(){
+        return tipoDeGasto;
+    }
+
+    float getValor(){
+        return valor;
+    }
+};
+
+Despensa::Despensa(){
+    valor = 0;
+}
+
+class ControledeGastos{
+private:
+    Despensa despesas[100];
+public:
+    ControledeGastos();
+    
+    void setDespesa(Despensa d, int pos);
+};
+
+ControledeGastos::ControledeGastos(){
+}
+void setDespesa(Despensa d, int pos){
+    d[pos];
+}
+
+
+
+int main(){
+
+    
+
+
+
+    return 0;
+}
+
