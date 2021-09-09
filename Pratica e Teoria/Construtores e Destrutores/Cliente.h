427532882cf0b61154fc0b9206cb4abde099880a
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-09-09 09:53:44 -0300

    up

--- Pratica e Teoria/Construtores e Destrutores/Cliente.h
+++ Pratica e Teoria/Construtores e Destrutores/Cliente.h
@@ -6,17 +6,48 @@ class Cliente{
 public:
     //Construtores
     Cliente();
-    Cliente(int cod);
-    Cliente(int cod, std::string n);
+    Cliente(int codigo);
+    Cliente(int codigo, std::string nome);
     //Destrutor
-    ~Cliente();
+    virtual ~Cliente();
+
+    void setSexo(std::string sexo);
+    std::string getSexo();
 
     int codigo;
     std::string nome;
+
+private:
+
+    std::string sexo;
 };
 
 
+Cliente::Cliente(){
+    this->codigo = 0;
+    this->nome = "VAZIO";
+}
 
+Cliente::Cliente(int codigo){
+    this->codigo = codigo;
+    this->nome = "VAZIO";
+}
+
+Cliente::Cliente(int codigo, std::string nome){
+    this->codigo = codigo;
+    this->nome = nome;
+}
+
+Cliente::~Cliente(){
+}
+
+void Cliente::setSexo(std::string sexo){
+    this->sexo = sexo;
+}
+
+std::string Cliente::getSexo(){
+    return this->sexo;
+}
 
 
 
--- Pratica e Teoria/Construtores e Destrutores/teste.cpp
+++ Pratica e Teoria/Construtores e Destrutores/teste.cpp
@@ -10,9 +10,18 @@ int main(){
     Cliente c2 = Cliente(12345);
     Cliente c3 = Cliente(12346, "Jose das Cruzes");
 
-    cout << c1.codigo << " - " << c1.nome << endl;
-    cout << c2.codigo << " - " << c2.nome << endl;
-    cout << c3.codigo << " - " << c3.nome << endl;
+    Cliente *p1;
+    p1 = new Cliente(12344, "Maria das Dores");
+    delete p1; // Libera espaço na memoria 
+    p1 = new Cliente(12344, "Jose Mateus");
+
+    c1.setSexo("Mulher");
+    c2.setSexo("Homem");
+    c3.setSexo("Trans");
+
+    cout << c1.codigo << " - " << c1.nome << " > " << c1.getSexo() <<  endl;
+    cout << c2.codigo << " - " << c2.nome << " > " << c2.getSexo() <<  endl;
+    cout << c3.codigo << " - " << c3.nome << " > " << c3.getSexo() <<  endl;
 
     return 0;
 }
