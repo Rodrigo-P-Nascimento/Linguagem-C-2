97b7b465caec62fd2fc2f575a78f7a8f12121a3c
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-09-09 19:09:12 -0300

    Create OOPessoa2.cpp

---
+++ LAB LP1/OOPessoa2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class OOPessoa1{ //Declarando a classe pessoa
+private:
+    string nome;
+    string telefone;
+    int idade;
+public:
+    //seteres
+    void setNome(string nome);
+    void setTelefone(string telefone);
+    void setIdade(int idade);
+    //Geteres
+    string getNome();
+    string getTelefone();
+    int getIdade();
+    //Constutores
+    OOPessoa1(string nome);
+    OOPessoa1(string nome, string telefone, int idade);
+};
+void OOPessoa1::setNome(string nome){
+    this->nome = nome;
+}
+void OOPessoa1::setTelefone(string telefone){
+    this->telefone = telefone;
+}
+void OOPessoa1::setIdade(int idade){
+    this->idade = idade;
+}
+OOPessoa1::OOPessoa1(string nome){
+    setNome(nome);
+}
+OOPessoa1::OOPessoa1(string nome, string telefone, int idade){
+    setNome(nome);
+    setTelefone(telefone);
+    setIdade(idade);
+}
+
+int main(){
+
+    int n, i, cont=0;
+    string name, cellphone, nomeProcurado;
+    int age;
+
+    cin >> n;
+
+    OOPessoa1 agenda[n]; //Iniciei logo os objetos com um valor padrão
+
+    for(i=0; i < n; i++){
+        cin.ignore();
+        getline(cin, name);
+        getline(cin, cellphone);
+        cin >> age;
+        agenda[i] = OOPessoa1(name, cellphone, age);
+    }
+
+    cin >> nomeProcurado;
+
+    for(i=0; i < n; i++){
+        if(agenda[i].getNome().find(nomeProcurado)){
+            cont = 1;
+            cout << agenda[i].getNome() << ", " << agenda[i].getIdade() << ", " << agenda[i].getTelefone() << endl;
+        }
+        if(i == (n-1) && cont == 0){
+            cout <<  "Pessoa não encontrada" << endl;
+        }
+    }
+
+    return 0;   
+}
