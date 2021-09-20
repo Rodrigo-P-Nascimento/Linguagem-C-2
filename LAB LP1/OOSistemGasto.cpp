5b586d7c9dfde372d286a5c82369007929b5162d
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-09-20 14:55:19 -0300

    Update OOSistemGasto.cpp

--- LAB LP1/OOSistemGasto.cpp
+++ LAB LP1/OOSistemGasto.cpp
@@ -42,7 +42,7 @@ string Despesa::gettipoDeGasto(){
 }
 //___ metodos ___
 Despesa::Despesa(){
-    valor = 0;
+    //valor = 0;
 }
 
 class ControleDeGastos{
@@ -57,14 +57,79 @@ public:
 };
 //___ Metodos ___
 void ControleDeGastos::setDespesa(Despesa d, int pos){
+    despesas[pos] = d;
+}
+Despesa ControleDeGastos::getDespesa(int pos){
+    return despesas[pos];
+}
+float ControleDeGastos::calculaTotalDeDespesas(){
+    float cont=0;
+
+    for(int i=0; i < 100; i++){
+        cont+= despesas[i].getValor();
+    }
+
+    return cont;
+}
+
+float ControleDeGastos::calculaTotalDeDespesas(string tipo){
+    float cont=0;
+
+    for(int i=0; i < 100; i++){
+        if(despesas[i].gettipoDeGasto() == tipo){
+            cont+= despesas[i].getValor();
+        }
+    }
+
+    return cont;
+}
+
+bool ControleDeGastos::existeDespesaDoTipo(string tipo){
+    bool existe = false;
+
+    for(int i=0; i < 100; i++){
+        if(despesas[i].gettipoDeGasto() == tipo){
+            existe = true;
+            cout << despesas[i].getNome() << ", R$ " << despesas[i].getValor() << endl;
+        }
+    }
     
+    return existe;
 }
 
 int main(){
 
-    ControleDeGastos despe[100];
+    ControleDeGastos despe;
+    Despesa aux;
 
+    int num, i;
+    string name, typeOfExpense;
+    float value;
 
+    cin >> num;
+    cin.ignore();
+
+    for(i=0; i < num; i++){
+        getline(cin, name);
+        cin >> value;
+        cin.ignore();
+        getline(cin, typeOfExpense);
+
+        aux.setNome(name);
+        aux.settipoDeGasto(typeOfExpense);
+        aux.setValor(value);
+
+        despe.setDespesa(aux, i);
+    }
+
+    getline(cin, typeOfExpense); // Estou reclicando a variavel para ser usada como parametro de pesquisa.
+
+    if(despe.existeDespesaDoTipo(typeOfExpense) == false){
+        cout << "Nenhuma despesa do tipo especificado" << endl;
+    }
+
+    cout << "Total: " << despe.calculaTotalDeDespesas(typeOfExpense) << "/" << despe.calculaTotalDeDespesas() << endl;
+    
     return 0;
 }
 
