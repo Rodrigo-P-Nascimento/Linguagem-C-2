469fdad762c9e2444c5279638042d737a319747a
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-09-30 13:59:59 -0300

    Create teste.cpp

---
+++ Pratica e Teoria/teste.cpp
@@ -0,0 +1,22 @@
+/*
+class DataHora{
+private:
+    //Composição
+    Data * estaData ; // instância da classe Data
+    Hora * estaHora ; // instância da classe Hora
+public:
+    //Construtores
+    DataHora (int hora, int min, int seg , int dia, int mes , int ano);
+    DataHora (int dia, int mes , int ano);
+}
+
+DataHora::DataHora (int hora, int min, int seg, int dia, int mes , int ano){
+    estaData = new Data(dia, mes, ano);
+    estaHora = new Hora(hora, min , seg);
+}
+DataHora::DataHora (int dia, int mes , int ano){
+    estaData = new Data(dia, mes , ano);
+    estaHora = new Hora(0, 0, 0);
+}
+
+*/
