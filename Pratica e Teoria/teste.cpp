668ff430fa3d57930e3e6d4646b7b8ead8247b34
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-10-04 14:00:25 -0300

    up

--- Pratica e Teoria/teste.cpp
+++ Pratica e Teoria/teste.cpp
@@ -20,3 +20,44 @@ DataHora::DataHora (int dia, int mes , int ano){
 }
 
 */
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+    virtual void v() { cout << "A virtual" << endl; }
+    void f() { cout << "A plain" << endl; }
+};
+
+class B : public A
+{
+public:
+    virtual void v() { cout << "B virtual" << endl; }
+    void f() { cout << "B plain" << endl; }
+};
+
+class C : public B
+{
+public:
+    virtual void v() { cout << "C virtual" << endl; }
+    void f() { cout << "C plain" << endl; }
+};
+
+int main(){
+    
+    A * a = new C;
+    a->f();
+    a->v();
+
+    ((B*)a)->f();
+    ((B*)a)->v();
+}
+
+/*
+A plain
+C virtual
+B plain
+C virtual
+*/
