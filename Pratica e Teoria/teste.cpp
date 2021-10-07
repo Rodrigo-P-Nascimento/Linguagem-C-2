65f57e669a979e5b1a06f9ffdba10968f395e24f
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-10-07 19:00:10 -0300

    up

--- Pratica e Teoria/teste.cpp
+++ Pratica e Teoria/teste.cpp
@@ -1,4 +1,4 @@
-
+/*
 #include <iostream>
 
 using namespace std;
@@ -34,9 +34,56 @@ int main(){
     ((B*)a)->v();
 }
 
-/*
 A plain
 C virtual
 B plain
 C virtual
+
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+int main(){
+
+    vector<int>num(5);
+
+    try{
+        num.at(6) = 10;
+        cout << num[6] << endl;
+    }catch(exception& e){ //a variavél | e | é só um placeholder pode ser qq outro nome
+        cout << "Erro: " << e.what() << endl; // what() retorna o tipo de erro!
+        //SAIDA: Erro: vector::_M_range_check: __n (which is 6) >= this->size() (which is 5)
+    }
+}
 */
+#include <iostream>
+
+using namespace std;
+
+double divide(double n1, double n2){
+    if(n2 == 0){
+        throw "Divisao por zero";
+    }
+    if(n1 >= 10){
+        throw "N1 precisa ser menor do que 10";
+    }
+
+    return n1/n2;
+}
+
+int main(){
+
+    double n1, n2;
+
+    cin >> n1 >> n2;
+
+    try{
+        cout << divide(n1, n2) << endl;
+    }
+    catch(const char* e){
+        cout << "Erro: " << e << '\n';
+    }
+}
