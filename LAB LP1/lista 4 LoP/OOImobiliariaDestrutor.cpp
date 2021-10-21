76ad24473288e21f9573f0c9850c33a8a7942940
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-10-21 10:45:39 -0300

    Update OOImobiliariaDestrutor.cpp

--- LAB LP1/lista 4 LoP/OOImobiliariaDestrutor.cpp
+++ LAB LP1/lista 4 LoP/OOImobiliariaDestrutor.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -40,6 +41,10 @@ void Imovel::setDisponibilidade(string disponibilidade){
     this->disponibilidade = disponibilidade;
 }
 
+void Imovel::exibeAtributos(){
+}
+void Imovel::lerAtributos(){
+}
 string Imovel::getNome(){
     return nome;
 }
@@ -58,7 +63,7 @@ Imovel::Imovel(){
 }
 Imovel::~Imovel(){
     cout << "Deletando Imovel" << endl;
-}
+}//End class Imovel
 
 class Casa : public Imovel{
 protected:
@@ -94,14 +99,130 @@ Casa::Casa()
 }
 Casa::~Casa(){
     cout << "Deletando Casa" << endl;
+}//end classe Casa
+
+class Apartamento : public Imovel{
+protected:
+    int andar;
+    int numQuartos;
+    int vagasGaragem;
+    float area;
+    float valorCondominio;
+public:
+    //Metodos
+    virtual void exibeAtributos();
+    virtual void lerAtributos();
+    //Destrutor e Construtor
+    Apartamento();
+    virtual ~Apartamento();
+};
+
+void Apartamento::exibeAtributos(){
+    cout << nome << " para " << disponibilidade << ". " << area << "m2 de área, " << numQuartos << " quartos, " << andar << " andar(es), " << valorCondominio << " de condomínio, " << vagasGaragem << " vaga(s) de garagem. R$ " << valor << "." <<endl;
+}
+void Apartamento::lerAtributos(){
+    cin >> valor;
+    cin.ignore();
+    getline(cin, disponibilidade);
+    cin >> area;
+    cin >> numQuartos;
+    cin >> andar;
+    cin >> valorCondominio;
+    cin >> vagasGaragem;
+}
+Apartamento::Apartamento()   
+    : Imovel(){
+    this->nome = "Apartamento";
+    this->tipo = 2;
+}
+Apartamento::~Apartamento(){
+    cout << "Deletando Apartamento" << endl;
+}//End class Apartamento
+
+class Terreno : public Imovel{
+protected:
+    float area;
+public:
+    //Metodos
+    virtual void exibeAtributos();
+    virtual void lerAtributos();
+    //Destrutor e Construtor
+    Terreno();
+    virtual ~Terreno();
+};
+
+void Terreno::exibeAtributos(){
+    cout << nome << " para " << disponibilidade << ". " << area << "m2 de área de terreno. R$ " << valor << "." << endl;
+}
+void Terreno::lerAtributos(){
+    cin >> valor;
+    cin.ignore();
+    getline(cin, disponibilidade);
+    cin >> area;
+}
+Terreno::Terreno()   
+    : Imovel(){
+    this->nome = "Terreno";
+    this->tipo = 3;
+}
+Terreno::~Terreno(){
+    cout << "Deletando Terreno" << endl;
+}//End class Terreno
+
+class Imobiliaria{
+protected:
+    vector<Imovel*> imoveis;
+public:
+    //Metodos
+    void addImovel(Imovel* imovel);
+    void buscaImovel(string busca);
+    //Construtores e Destrutores
+    Imobiliaria();
+    ~Imobiliaria();
+};
+void Imobiliaria::addImovel(Imovel* imovel){
+    imoveis.push_back(imovel);
+}
+void Imobiliaria::buscaImovel(string busca){
+    for(auto& it : imoveis){
+        if(it->getNome().find(busca) != string::npos || it->getDisponibilidade().find(busca) != string::npos){
+            it->exibeAtributos();
+        }
+    }
+}
+Imobiliaria::Imobiliaria(){
 }
 
+Imobiliaria::~Imobiliaria(){
+    cout << "Deletando Imobiliária" << endl;
+    for(auto& it : imoveis){
+        it->~Imovel();
+    }
+    imoveis.clear();
+}//End class Imobiliaria
 
 int main(){
 
-    Casa c1 = Casa();
+    int n, tipo;
+    string busca;
+    Imobiliaria im;
+    Imovel *imov;
 
-    cout << "opa" << endl;
+    cin >> n;
 
+    for(int i=0; i < n; i++){
+        cin >> tipo;
+
+        if(tipo == 1) imov = new Casa();
+        else if(tipo == 2) imov = new Apartamento();
+        else if(tipo == 3) imov = new Terreno();
+
+        imov->lerAtributos();
+        im.addImovel(imov);
+    }
+    cin.ignore();
+    getline(cin, busca);
+    im.buscaImovel(busca);
+    
     return 0;
 }
