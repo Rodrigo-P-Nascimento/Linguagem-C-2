c200b5c856cd1d57c46dcc079c1cc2ed998d6403
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-11-25 14:55:03 -0300

    up

--- LAB LP1/lista 4 LoP/teste.r
+++ LAB LP1/lista 4 LoP/teste.r
@@ -1,17 +1,18 @@
+library(matlib)
+
 x <- seq(-1,1,by=0.2)#dados
-y <- c(0.8517503, 1.8462013, 5.4867843, −4.7784345,-1.7507537, −0.8230178, −0.7315276, −0.4738893, −0.4134608, −0.3333150, −0.2770552)#dados
+y <- c(20.9611920, 15.2826605, 10.3260988, 6.6129452, 4.9051565, 2.7211060, 1.9869818, 1.4774476, 0.8526788, 0.6614945, 0.3759807)#dados
 
-a1 <- (mean(x*y) - mean(x)*mean(y))/(mean(x^2) - mean(x)^2)#coeficiente angular MMQ
-b1 <- (mean(y)*mean(x^2) - mean(x)*mean(x*y))/(mean(x^2) - mean(x)^2)#coeficiente linear MMQ
+Y <- log(y)
+#Regressão linear
+a1 <- (mean(x*Y) - mean(x)*mean(Y))/(mean(x^2) - mean(x)^2)#coeficiente angular MMQ
+b1 <- (mean(Y)*mean(x^2) - mean(x)*mean(x*Y))/(mean(x^2) - mean(x)^2)#coeficiente linear MMQ
 
-a2 <- (mean(x*(1/y)) - mean(x)*mean((1/y)))/(mean(x^2) - mean(x)^2)
-b2 <- (mean((1/y))*mean(x^2) - mean(x)*mean(x*(1/y)))/(mean(x^2) - mean(x)^2)
+#Grafico
+plot(x, y,pch = 19, cex = 0.7)
+curve(exp(b1)*exp(a1*x), -1, 1,type = "l", col = "blue", add = T)#grafico da curva exponencial
+legend(x = "topleft", legend = "Regressão exponencial", col = "blue", lty  = 1, lwd = 1, bty = "n")#legenda
 
-plot(x,y,pch=19,cex = 0.7)
-
-curve(a1*x + b1,-1,1,type = "l", col = "blue", add = T)#reta ajustada
-curve (1/(a2*x + b2) ,-1,1,type = "l",col = "red",add = T)#hiperbole
-
-legend(x = "topright", legend = "Modelo hiperbólico", col = "red", lty = 1, lwd = 1, bty = "n")#legenda
-legend(x = "topleft", legend = "Regressão Linear", col = "blue", lty = 1, lwd = 1, bty = "n")#legenda
-
+#Soma dos resíduos quadráticos
+res2 <- sum((exp(b1)*exp(a1*x) - y)^2)
+res2
