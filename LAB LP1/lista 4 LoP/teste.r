4738abcba4e0a13a76160928ac7e5c07d6317fa1
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-11-26 11:01:11 -0300

    up

--- LAB LP1/lista 4 LoP/teste.r
+++ LAB LP1/lista 4 LoP/teste.r
@@ -1,19 +1,19 @@
 library(matlib)
 
-x <- seq(-1, 1, by=0.2)#dados
-y <- c(5.1213515, 3.9260492, 3.3917448, 2.4160260, 2.1235133, 1.2816421, 1.1253662, 0.8549460, 0.6753195, 0.7752061, 0.9619687)#dados
+x <- seq(-1,1,by=0.2)#dados
+y <- c(1.325797, 2.088424, 2.441511, 2.041860, 2.602180, 3.527601, 3.996762, 4.142978, 5.097782, 5.647629, 5.785799)#dados
 
-#Sistema de equações normais
-N <- length(x)#N será o numero de dados 
-A <- matrix(c(N,sum(x), sum(x^2), sum(x), sum(x^2), sum(x^3), sum(x^2), sum(x^3), sum(x^4)), 3,3)#matriz
-b <- c(sum(y), sum(x*y), sum(x^2*y))#vetor
-c <- solve(A, b)#solução
+Y <- log(y)
+#Regressão linear
+a1 <- (mean(x*Y) - mean(x)*mean(Y))/(mean(x^2) - mean(x)^2)#coeficiente angular MMQ
+b1 <- (mean(Y)*mean(x^2) - mean(x)*mean(x*Y))/(mean(x^2) - mean(x)^2)#coeficiente linear MMQ
 
 #Grafico
-plot(x, y, pch = 19, cex = 0.7)
-curve(c[3]*x^2 + c[2]*x + c[1], -1, 1, type = "l", col = "blue", add = T)#grafico dados
-legend(x = "topright", legend = "Regressão quadrática", col = "blue", lty = 1, lwd = 1, bty = "n")#legenda
+plot(x, y,pch = 19, cex = 0.7)
+curve(exp(b1 + a1*x), -1, 1,type = "l", col = "blue", add = T)#grafico da curva exponencial
+legend(x = "topleft", legend = "Curva exponencial", col = "blue", lty  = 1, lwd = 1, bty = "n")#legenda
 
-#Soma dos resíduos quadráticas
-res2 <- sum((c[3]*x^2 + c[2]*x + c[1] - y)^2)
+#Soma dos resíduos quadráticos
+res2 <- sum((exp(b1 + a1*x) - y)^2)
 res2
+
