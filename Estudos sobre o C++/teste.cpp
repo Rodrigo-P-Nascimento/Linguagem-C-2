5ea48b1e6e12d63f5004d349f7f58b4ee4f79075
Rodrigo <rpngithub001@gmail.com>
2023-05-14 08:45:32 -0300

    Create teste.cpp

---
+++ Estudos sobre o C++/teste.cpp
@@ -0,0 +1,24 @@
+
+[0,1,3,5,10] soluçao
+
+i = 0
+j = 4
+
+trocar melhor 2 e 5
+
+for (i=0; i < n; i++){
+    for(j = 0; j < n; j++ ){
+
+        if(j = i){
+            nada
+        }
+        else{
+            s1 = copia(s)   
+            SWAP(i,j,s1)
+            valor = f(s)
+
+            if s1 melhor do s:
+                s = s1;
+        }
+    }
+}
